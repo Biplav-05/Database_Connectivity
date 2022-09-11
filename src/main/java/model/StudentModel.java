20d46ce1bed5d1ad84a118a1e6442a1ea8f440e9
Biplav-05 <biplav413@gmail.com>
2022-09-11 16:21:02 +0545

    database task

---
+++ src/main/java/model/StudentModel.java
@@ -0,0 +1,28 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class StudentModel {
+    @Id
+    @SequenceGenerator(name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence")
+private Long id;
+private String name;
+private String address;
+private String email;
+}
